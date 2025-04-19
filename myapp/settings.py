f3ab8f91a1dd65345e170f63727777c9ce7b2abb
adhish1478 <adhish.aravind7@gmail.com>
2025-04-19 09:31:44 +0530

    blogpostapi live with postgress

--- myapp/settings.py
+++ myapp/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "BlogPostAPI",
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost', 
     }
 }
 
