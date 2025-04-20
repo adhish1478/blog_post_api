82ee8ca1fca5506d455b9f9c3a75c2ff21e7e1eb
adhish1478 <adhish.aravind7@gmail.com>
2025-04-20 20:00:46 +0530

    update blogpost func

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
+    path('blogposts/<int:pk>/', views.BlogPostRetrieveUpdateDestroy.as_view(), name= 'update'),
     ]
